8f5f4e356e6e363b35f78e903b377855710acc8a
Albert Bogdanowicz <albert.bogdanowicz@gmail.com>
2015-12-02 22:46:50 +0100

    File for helper functions, starts with a function for downloading single page source

---
+++ crawler_functions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import urllib.request
+import urllib.parse
+
+
+BASE_URL = "http://tvtropes.org/pmwiki/pmwiki.php/"
+URL_QUERY = "?action=source"
+
+
+
+def download_page_source(title, namespace="Main"):
+    url = BASE_URL + urllib.parse.quote(namespace.replace(' ', '') + '/' + title) + URL_QUERY
+    with urllib.request.urlopen(url) as request:
+        source = request.read()
+    return source
