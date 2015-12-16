ab81d8da4255ac18e567614b88bc572bf3ccd5b3
Albert Bogdanowicz <albert.bogdanowicz@gmail.com>
2015-12-16 20:22:40 +0100

    Downloading deeper links

--- crawler_functions.py
+++ crawler_functions.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import urllib.request
 import urllib.parse
+import re
 
 
 BASE_URL = "http://tvtropes.org/pmwiki/pmwiki.php/"
 URL_QUERY = "?action=source"
+DEEP_LINK_PATTERN = re.compile('/Tropes.To.')
 
 
 def sanitize_link(link_txt):
@@ -35,7 +37,21 @@ def get_subindexes_from_index(page_src):
 
 
 def get_entries_from_page(page_src):
-    # TODO: TropesAToC...
     page_src = page_src.split('----')[1]
     page_lines = page_src.split('<br>')
     return [sanitize_link(i) for i in page_lines if i.startswith('*')]
+
+
+def check_deep_link(link_txt):
+    return DEEP_LINK_PATTERN.search(link_txt) is not None
+
+
+def deep_get_entries_from_page(page_src):
+    links = get_entries_from_page(page_src)
+    # TODO: I think we don't need a loop here, because links are not nested that much
+    deep_links = [i for i in links if check_deep_link(i)]
+    for i in deep_links:
+        deep_page_src = download_page_source(i.split('/')[1], i.split('/')[0])  # TODO: it's ugly
+        links.extend(get_entries_from_page(deep_page_src))
+    links = [i for i in links if not check_deep_link(i)]
+    return links
